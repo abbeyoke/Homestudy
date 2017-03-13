5a3c577e6d96b468ea7619db24dcbf5e87a69240
Abiodun Oketoki <abiodunoketoki@gmail.com>
2017-03-13 17:07:22 +0100

    oop still in progress. 2 tests passed.

---
+++ oop class.js
@@ -0,0 +1,7 @@
+function car(name, model){
+	this.name = 'General';
+	this.model = 'GM';
+	this.type;
+}
+
+var toyota = new car('Toyota', 'Corolla');  
---
+++ oop/spec/SpecHelper.js
@@ -0,0 +1,25 @@
+/*beforeEach(function () {
+  jasmine.addMatchers({
+    toBePlaying: function () {
+      return {
+        compare: function (actual, expected) {
+          var player = actual;
+
+          return {
+            pass: player.currentlyPlayingSong === expected && player.isPlaying
+          };
+        }
+      };
+    }
+  });
+});*/
+
+//find min
+
+var Car = function (name, model){
+	this.name = 'General';
+	this.model = 'GM';
+	this.type;
+}
+
+var toyota = new car('Toyota', 'Corolla');  
